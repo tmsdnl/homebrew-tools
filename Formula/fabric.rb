e662642f91dc1d35f0f012c592270564558fef31
Tomas Danila <tomas@tmsdnl.com>
2024-11-26 20:51:52 -0800

    Add fabric formula

---
+++ Formula/fabric.rb
@@ -0,0 +1,27 @@
+class Fabric < Formula
+  desc "Open-source framework for augmenting humans using AI."
+  homepage "https://github.com/danielmiessler/fabric"
+  version "1.4.114"
+  license "MIT"
+
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/danielmiessler/fabric/releases/latest/download/fabric-darwin-arm64"
+      sha256 "e411ef923bacbee726fa57303be5501470d6cb93e6bc28748ba638af0a6d6d61"
+    else
+      url "https://github.com/danielmiessler/fabric/releases/latest/download/fabric-darwin-amd64"
+      sha256 "beb210559aba94eaac7c80a4474587392b6e56017e7a6b1b31d4ea6aacd88667"
+    end
+  else
+    odie "Fabric is currently only supported on macOS."
+  end
+
+  def install
+    bin.install "fabric"
+    chmod "+x", bin/"fabric"
+  end
+
+  test do
+    assert_match "Fabric", shell_output("#{bin}/fabric --version")
+  end
+end
