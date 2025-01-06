ba7783e62bc2aeea12fe35bbb829d7bd6d420319
Tomas Danila <tomas@tmsdnl.com>
2025-01-05 21:55:26 -0800

    fix(apiparrot): Move to Casks

--- Casks/apiparrot.rb
+++ Casks/apiparrot.rb
@@ -1,6 +1,6 @@
 cask "apiparrot" do
     version "0.2.3"
-    sha256 "65a7e39a52c9e9a2bb5265ba340e1e5655b7ad02" # Replace with the actual SHA256 hash
+    sha256 "65a7e39a52c9e9a2bb5265ba340e1e5655b7ad02"
   
     url "https://github.com/apiparrot/apiparrot-desktop-releases/releases/download/v#{version}/API_Parrot-#{version}.dmg"
     name "API Parrot"
