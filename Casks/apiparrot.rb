521f5a90b7287dfc9643e9442134f287fc4eb658
Tomas Danila <tomas@tmsdnl.com>
2025-01-05 21:58:01 -0800

    fix(apiparrot): Append -universal to dmg name

--- Casks/apiparrot.rb
+++ Casks/apiparrot.rb
@@ -2,7 +2,7 @@ cask "apiparrot" do
     version "0.2.3"
     sha256 "65a7e39a52c9e9a2bb5265ba340e1e5655b7ad02"
   
-    url "https://github.com/apiparrot/apiparrot-desktop-releases/releases/download/v#{version}/API_Parrot-#{version}.dmg"
+    url "https://github.com/apiparrot/apiparrot-desktop-releases/releases/download/v#{version}/API_Parrot-#{version}-universal.dmg"
     name "API Parrot"
     desc "Tool for reverse engineering HTTP APIs"
     homepage "https://apiparrot.com/"
