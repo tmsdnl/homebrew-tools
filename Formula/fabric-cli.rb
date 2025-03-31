625eb70b087a10ec50de5d4e23fe24221fa6aecf
Tomas Danila <tomas@tmsdnl.com>
2025-03-31 08:41:36 -0700

    chore(fabric-cli): v1.4.167

--- Formula/fabric-cli.rb
+++ Formula/fabric-cli.rb
@@ -1,16 +1,16 @@
 class FabricCli < Formula
   desc "Open-source framework for augmenting humans using AI."
   homepage "https://github.com/danielmiessler/fabric"
-  version "v1.4.166"
+  version "v1.4.167"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/danielmiessler/fabric/releases/latest/download/fabric-darwin-arm64"
-      sha256 "5c1022e0fe9b58b395b8aff8079e02caef69be9437dc8e6bd7037a74543f6aa9"
+      sha256 "1eac13a6d37124789f5a7d1eba19a4055bbc6be7acba1b12eb315a595e9a4296"
     else
       url "https://github.com/danielmiessler/fabric/releases/latest/download/fabric-darwin-amd64"
-      sha256 "2d07835c352262eba247114c697940f139b77bdc9c8f78b30cb83505e28fe2ab"
+      sha256 "a8da00e4d953825ee02055d57a23ef21f97da5fce1e8f4ed43849df81abd5a44"
     end
   else
     odie "Fabric is currently only supported on macOS."
